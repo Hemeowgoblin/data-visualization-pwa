8a54acdbc1fc68cb7539c160e0704d5282459eb7
Hemeowgoblin <mahairi.ebrahim@gmail.com>
2026-04-01 12:15:48 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Data Visualization Website
+
+This repository is for the Data Visualization PWA project.
---
+++ setup_git.sh
@@ -0,0 +1,6 @@
+git init
+git remote add origin https://github.com/Hemeowgoblin/data-visualization-pwa
+git add .
+git commit -m "initial commit"
+git branch -M main
+git push -u origin main
